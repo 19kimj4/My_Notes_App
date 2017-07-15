72d66ff468f29b51ea0016eb346aac9e1774c02b
Ji Yeon Kim <19kimj4@wis.edu.hk>
2017-07-15 20:24:23 +0800

    Add completed

---
+++ MakeSchoolNotes-Swift-V2-Starter-swift3-coredata/MakeSchoolNotes/Models/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  MakeSchoolNotes
+//
+//  Created by Ji Yeon Kim on 14/7/2017.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var title = ""
+     var content = ""
+    var modificationTime = Date()
+}
